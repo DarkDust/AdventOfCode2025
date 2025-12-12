f386471af3d9e924c809f001d695f076cb1bfc04
Marc Haisenko <haisenko@equinux.com>
2025-12-12 12:05:04 +0100

    Improved part 2, now finds solution in 16min
    Well, at least it works…

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -9,6 +9,7 @@ enum Error {
     NoSolution,
 }
 
+#[derive(PartialEq)]
 enum EvalResult {
     Incomplete,
     Hit,
@@ -70,7 +71,7 @@ impl Machine {
 
     fn light_up(&self) -> Result<usize, Error> {
         // Brute-force recursive search. Pretty stupid, but finishes in less than a second so OK for now.
-        let start_lights = [false].repeat(self.lights.len());
+        let start_lights = vec![false; self.lights.len()];
         if start_lights == self.lights {
             return Ok(0);
         }
@@ -117,28 +118,70 @@ impl Machine {
         return lights;
     }
 
-    fn select_buttons(&self, buttons: &Vec<Button>, position: usize) -> (Vec<Button>, Vec<Button>) {
+    fn button_is_valid(button: &Vec<usize>, invalid_positions: &Vec<usize>) -> bool {
+        for pos in button {
+            if invalid_positions.contains(pos) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fn select_buttons(
+        &self,
+        buttons: &Vec<Button>,
+        current_joltage: &Vec<usize>,
+    ) -> (Vec<Button>, Vec<Button>, usize) {
+        // Search position affected by the least number of buttons greater than zero.
+
+        let mut least_num_buttons = usize::MAX;
+        let mut least_position = usize::MAX;
+        let mut invalid_positions = Vec::new();
+
+        for pos in 0..self.joltage.len() {
+            let target = self.joltage[pos];
+            let current = current_joltage[pos];
+            if current >= target {
+                // Position already satisfied, must not consider buttons affecting it.
+                invalid_positions.push(pos);
+                continue;
+            }
+        }
+
+        for pos in 0..self.joltage.len() {
+            let num_buttons = buttons
+                .iter()
+                .filter(|button| {
+                    button.contains(&pos) && Machine::button_is_valid(button, &invalid_positions)
+                })
+                .count();
+            if num_buttons > 0 && num_buttons < least_num_buttons {
+                least_num_buttons = num_buttons;
+                least_position = pos;
+            }
+        }
+
+        if least_position == usize::MAX {
+            // No buttons left to press, we're done.
+            return (Vec::new(), buttons.clone(), 0);
+        }
+
         let mut selected = Vec::new();
         let mut remaining = Vec::new();
 
         for button in buttons {
-            if button.contains(&position) {
+            if button.contains(&least_position) {
                 selected.push(button.clone());
             } else {
                 remaining.push(button.clone());
             }
         }
 
-        return (selected, remaining);
-    }
+        let target = self.joltage[least_position];
+        let current = current_joltage[least_position];
+        assert!(target > current);
 
-    fn max_repetitions(&self, current_joltage: &Vec<usize>, position: usize) -> Option<usize> {
-        let target = self.joltage[position];
-        let current = current_joltage[position];
-        if current > target {
-            return None;
-        }
-        return Some(target - current);
+        return (selected, remaining, target - current);
     }
 
     fn evaluate_joltage(&self, current_joltage: &Vec<usize>) -> EvalResult {
@@ -147,9 +190,9 @@ impl Machine {
 
         for pos in 0..joltage_len {
             match self.joltage[pos].cmp(&current_joltage[pos]) {
-                Ordering::Less => continue,
+                Ordering::Greater => continue,
                 Ordering::Equal => hit += 1,
-                Ordering::Greater => return EvalResult::Invalid,
+                Ordering::Less => return EvalResult::Invalid,
             }
         }
 
@@ -166,111 +209,62 @@ impl Machine {
         }
     }
 
-    // Calculate all possible combinations of indices/positions that add up to a given count.
-    fn index_combinations(&self, positions: usize, count: usize) -> Vec<Vec<usize>> {
-        fn helper(
-            positions: usize,
-            count: usize,
-            current: &mut Vec<usize>,
-            out: &mut Vec<Vec<usize>>,
-        ) {
-            if positions == 1 {
-                // Only one position left, take shortcut.
-                let mut result = current.clone();
-                result.push(count);
-                out.push(result);
-                return;
-            }
-
-            // Try all values for the current position: 0..=count
-            for i in 0..=count {
-                // Add the current value for the current position.
-                current.push(i);
-                // Recurse to handle the next position.
-                helper(positions - 1, count - i, current, out);
-                current.pop();
-            }
+    // Much improved version to get the index combinations.
+    // Stolen from https://github.com/michel-kraemer/adventofcode-rust/blob/main/2025/day10/src/main.rs
+    fn next_combination(&self, combinations: &mut [usize]) -> bool {
+        let i = combinations.iter().rposition(|&v| v != 0).unwrap();
+        if i == 0 {
+            return false;
         }
-
-        let mut result = Vec::new();
-        let mut current = Vec::new();
-        helper(positions, count, &mut current, &mut result);
-
-        return result;
-    }
-
-    // Sort the position indices by joltage value, descending (for `pop()`ing the smallest value first).
-    fn sort_positions(&self) -> Vec<usize> {
-        let mut enumerated = self
-            .joltage
-            .iter()
-            .enumerate()
-            .collect::<Vec<(usize, &usize)>>();
-        enumerated.sort_by_key(|pos| -(*pos.1 as isize));
-        let positions = enumerated
-            .into_iter()
-            .map(|pos| pos.0)
-            .collect::<Vec<usize>>();
-        return positions;
+        let v = combinations[i];
+        combinations[i - 1] += 1;
+        combinations[i] = 0;
+        combinations[combinations.len() - 1] = v - 1;
+        true
     }
 
     fn search_joltage(
         &self,
         current_joltage: &Vec<usize>,
-        positions: &Vec<usize>,
         buttons: &Vec<Button>,
         presses: usize,
+        least_presses: &mut usize,
     ) -> Option<usize> {
-        let mut updated_positions = positions.clone();
-        let position: usize;
-        if let Some(value) = updated_positions.pop() {
-            position = value;
-        } else {
-            // Actually, if we get here, the result MUST be valid?
-            match self.evaluate_joltage(current_joltage) {
-                EvalResult::Hit => return Some(presses),
-                EvalResult::Incomplete => return None,
-                EvalResult::Invalid => return None,
-            }
+        let (selected_buttons, remaining_buttons, max_presses) =
+            self.select_buttons(buttons, current_joltage);
+
+        if (presses + max_presses) > *least_presses {
+            // More expensive than the best solution we've found so far.
+            return None;
         }
 
-        let count: usize;
-        match self.max_repetitions(current_joltage, position) {
-            Some(value) => count = value,
-            None => {
-                // We've hit a dead end and overflowed the current position.
-                return None;
-            }
-        }
-
-        if count == 0 {
-            // Position is already at the target value, advance to next position.
-            return self.search_joltage(current_joltage, &updated_positions, buttons, presses);
-        }
-
-        let (selected_buttons, remaining_buttons) = self.select_buttons(buttons, position);
         match selected_buttons.len() {
             0 => {
-                // No buttons left,
-                return None;
+                // No buttons left, we're done.
+                match self.evaluate_joltage(current_joltage) {
+                    EvalResult::Hit => return Some(presses),
+                    EvalResult::Incomplete => return None,
+                    EvalResult::Invalid => return None,
+                }
             }
             1 => {
-                // Only one button left.
+                // Only one button left, shortcut.
                 let button = &selected_buttons[0];
                 let mut updated_joltage = current_joltage.clone();
-                self.push_button_joltage(&mut updated_joltage, button, count);
+                self.push_button_joltage(&mut updated_joltage, button, max_presses);
                 return self.search_joltage(
                     &updated_joltage,
-                    &updated_positions,
                     &remaining_buttons,
-                    presses + count,
+                    presses + max_presses,
+                    least_presses,
                 );
             }
             _ => {
                 // Try permutations.
-                let index_combinations = self.index_combinations(selected_buttons.len(), count);
+                let mut index_combination = vec![0; selected_buttons.len() - 1];
+                index_combination.push(max_presses);
                 let mut best = None;
-                for index_combination in index_combinations {
+                loop {
                     let mut updated_joltage = current_joltage.clone();
                     for (index, pushes) in index_combination.iter().enumerate() {
                         self.push_button_joltage(
@@ -279,18 +273,44 @@ impl Machine {
                             *pushes,
                         );
                     }
-                    match self.search_joltage(
-                        &updated_joltage,
-                        &updated_positions,
-                        &remaining_buttons,
-                        presses + count,
-                    ) {
-                        None => continue,
-                        Some(recursed_pressed) => {
-                            if best.is_none() || recursed_pressed < best.unwrap() {
-                                best = Some(recursed_pressed);
+
+                    match self.evaluate_joltage(&updated_joltage) {
+                        EvalResult::Hit => {
+                            // A solution was found!
+                            let did_press = presses + max_presses;
+                            if best.is_none() || did_press < best.unwrap() {
+                                best = Some(did_press);
+                                if did_press < *least_presses {
+                                    *least_presses = did_press;
+                                }
                             }
                         }
+                        EvalResult::Incomplete => {
+                            // Not done yet, recurse.
+                            match self.search_joltage(
+                                &updated_joltage,
+                                &remaining_buttons,
+                                presses + max_presses,
+                                least_presses,
+                            ) {
+                                None => {}
+                                Some(recursed_pressed) => {
+                                    if best.is_none() || recursed_pressed < best.unwrap() {
+                                        best = Some(recursed_pressed);
+                                        if recursed_pressed < *least_presses {
+                                            *least_presses = recursed_pressed;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        EvalResult::Invalid => {
+                            // Overshot, try next combination.
+                        }
+                    }
+
+                    if !self.next_combination(&mut index_combination) {
+                        break;
                     }
                 }
                 return best;
@@ -299,9 +319,9 @@ impl Machine {
     }
 
     fn best_joltage(&self) -> Result<usize, Error> {
-        let start_joltage = [0].repeat(self.joltage.len());
-        let start_buttons = self.sort_positions();
-        match self.search_joltage(&start_joltage, &start_buttons, &self.buttons, 0) {
+        let start_joltage = vec![0; self.joltage.len()];
+        let mut least_presses = usize::MAX;
+        match self.search_joltage(&start_joltage, &self.buttons, 0, &mut least_presses) {
             Some(value) => Ok(value),
             None => Err(Error::NoSolution),
         }
